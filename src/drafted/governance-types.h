e9ba954129ecd3a84318975a30b4c57ef7355e7c
ParadigmCurrency <35574937+ParadigmCurrency@users.noreply.github.com>
2018-01-23 10:24:18 -0500

    Update governance-types.h

--- src/drafted/governance-types.h
+++ src/drafted/governance-types.h
@@ -16,9 +16,9 @@ enum GovernanceObjectType {
     ValueOverride = 1, 
 
     // -------------------------------
-    // DashNetwork - is the root node
-    DashNetwork = 1000,
-    DashNetworkVariable = 1001,
+    // ParadigmNetwork - is the root node
+    ParadigmNetwork = 1000,
+    ParadigmNetworkVariable = 1001,
     Category = 1002,
 
     // Actors
