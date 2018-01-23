0495c9f0a20a24e366d1a05d99e42ed582174689
ParadigmCurrency <35574937+ParadigmCurrency@users.noreply.github.com>
2018-01-23 11:52:17 -0500

    Update macnotificationhandler.mm

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.dash.Dash-Qt";
+        return @"org.paradigm.Paradigm-Qt";
     } else {
         return [self __bundleIdentifier];
     }
